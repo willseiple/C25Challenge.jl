eadc8dc4bf3a6e37edf4acb40d0c1244bb4842b0
Documenter.jl <documenter@juliadocs.github.io>
2022-11-26 04:08:16 +0000

    build based on dd29c8f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = C25Challenge","category":"page"},{"location":"#C25Challenge","page":"Home","title":"C25Challenge","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for C25Challenge.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [C25Challenge]","category":"page"}]
+}
